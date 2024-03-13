ccde2a564803b74f1c33233b32bd56d4243ad474
Lsn-User <Lsn-User@DESKTOP-GLKVRGN>
2024-03-13 21:51:06 +0530

    Normal project comit

---
+++ src/exampleGit/Examplegit.java
@@ -0,0 +1,11 @@
+package exampleGit;
+
+public class Examplegit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hai,How are you");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module exampleGit {
+}
